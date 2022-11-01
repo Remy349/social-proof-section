c6cdd93af0c959c80ec8ca53e6fc2d9dce7218fa
Remy349 <santoraga15@gmail.com>
2022-10-31 23:25:58 -0600

    Working on the structure of the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { StarsInfo } from './components/StarsInfo'
 
 function App () {
   return (
-    <main className='main'>
-      <h1>OK!!!</h1>
+    <main className='social'>
+      <div className='social__info'>
+        <div className='social__info-content'>
+          <h1 className='social__info-content_title'>
+            10,000+ of our users love our products.
+          </h1>
+          <p className='social__info-content_desc'>
+            We only provide great products combined with excellent customer service.
+            See what our satisfied customers are saying about our services.
+          </p>
+        </div>
+        <div className='social__info-stars'>
+          <StarsInfo
+            rated='Rated 5 Stars in Reviews'
+          />
+          <StarsInfo
+            rated='Rated 5 Stars in Report Guru'
+          />
+          <StarsInfo
+            rated='Rated 5 Stars in BestTech'
+          />
+        </div>
+      </div>
+      <div className='social__cards'></div>
     </main>
   )
 }
---
+++ src/components/StarsInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const StarsInfo = (props) => {
+  return (
+    <p className='social__info-stars_rated'>
+      {props.rated}
+    </p>
+  )
+}
